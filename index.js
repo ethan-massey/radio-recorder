cf4acf81312daac475ba668520252529f2d2f9a6
Ethan Massey <ejm4aa@virginia.edu>
2023-03-08 16:41:28 -0500

    refactor

--- index.js
+++ index.js
@@ -1,35 +1,41 @@
-// Import module.
+// Imports modules.
+const fs = require('fs'), path = require('path')
 const AudioRecorder = require('node-audiorecorder')
+const { getFormattedCurrentDate } = require('./dateFormat')
 
-// Options is an optional parameter for the constructor call.
-// If an option is not given the default value, as seen below, will be used.
-const options = {
-  program: `rec`, // Which program to use, either `arecord`, `rec`, or `sox`.
-  device: null, // Recording device to use, e.g. `hw:1,0`
+// Constants.
+const DIRECTORY = 'example-recordings';
 
-  bits: 16, // Sample size. (only for `rec` and `sox`)
-  channels: 1, // Channel count.
-  encoding: `signed-integer`, // Encoding type. (only for `rec` and `sox`)
-  format: `S16_LE`, // Encoding type. (only for `arecord`)
-  rate: 16000, // Sample rate.
-  type: `wav`, // Format type.
-
-  // Following options only available when using `rec` or `sox`.
-  silence: 2, // Duration of silence in seconds before it stops recording.
-  thresholdStart: 0.5, // Silence threshold to start recording.
-  thresholdStop: 0.5, // Silence threshold to stop recording.
-  keepSilence: true, // Keep the silence in the recording.
+// Create path to write recordings to.
+if (!fs.existsSync(DIRECTORY)) {
+  fs.mkdirSync(DIRECTORY);
 }
-// Optional parameter intended for debugging.
-// The object has to implement a log and warn function.
-const logger = console
 
-// Create an instance.
-let audioRecorder = new AudioRecorder(options, logger)
+// Initialize recorder and file stream.
+const audioRecorder = new AudioRecorder({
+  program: 'sox',
+  rate: 44100,
+  silence: 0
+}, console);
 
-// Creates and starts the recording process.
-audioRecorder.start()
-// Stops and removes the recording process.
-audioRecorder.stop()
-// Returns the stream of the recording process.
-audioRecorder.stream()
+// Log information on the following events.
+audioRecorder.on('error', function () {
+  console.warn('Recording error.');
+});
+audioRecorder.on('end', function () {
+  console.warn('Recording ended.');
+});
+
+// Create file path
+const fileName = [DIRECTORY,'/',getFormattedCurrentDate(),'.wav'].join('')
+console.log('Writing new recording file at:', fileName);
+
+// Create write stream.
+const fileStream = fs.createWriteStream(fileName, { encoding: 'binary' });
+
+// Start and write to the file.
+audioRecorder.start().stream().pipe(fileStream);
+
+// Keep process alive.
+process.stdin.resume();
+console.warn('Press ctrl+c to exit.');
