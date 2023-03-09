aa73af6b7b08e8a28f4ade3232d4c197c73df293
Ethan Massey <ejm4aa@virginia.edu>
2023-03-09 00:16:43 -0500

    added cron

--- index.js
+++ index.js
@@ -2,45 +2,54 @@
 const fs = require('fs'), path = require('path')
 const AudioRecorder = require('node-audiorecorder')
 const { getFormattedCurrentDate } = require('./dateFormat')
+var CronJob = require('cron').CronJob
 
-// Constants.
-const DIRECTORY = 'example-recordings';
+var job = new CronJob(
+	'45 5 * * 1-5',
+	function() {
+		// Constants.
+    const DIRECTORY = 'example-recordings';
 
-// Create path to write recordings to.
-if (!fs.existsSync(DIRECTORY)) {
-  fs.mkdirSync(DIRECTORY);
-}
+    // Create path to write recordings to.
+    if (!fs.existsSync(DIRECTORY)) {
+      fs.mkdirSync(DIRECTORY);
+    }
 
-// Initialize recorder and file stream.
-const audioRecorder = new AudioRecorder({
-  program: 'sox',
-  rate: 44100, // 44.1kHz CD sample rate standard
-  silence: 0
-}, console);
+    // Initialize recorder and file stream.
+    const audioRecorder = new AudioRecorder({
+      program: 'sox',
+      rate: 44100, // 44.1kHz CD sample rate standard
+      silence: 0
+    }, console);
 
-// Log information on the following events.
-audioRecorder.on('error', function () {
-  console.warn('Recording error.');
-});
-audioRecorder.on('end', function () {
-  console.warn('Recording ended.');
-});
+    // Log information on the following events.
+    audioRecorder.on('error', function () {
+      console.warn('Recording error.');
+    });
+    audioRecorder.on('end', function () {
+      console.warn('Recording ended.');
+    });
 
-// Create file path
-const fileName = [DIRECTORY,'/',getFormattedCurrentDate(),'.wav'].join('')
-console.log('Writing new recording file at:', fileName);
+    // Create file path
+    const fileName = [DIRECTORY,'/',getFormattedCurrentDate(),'.wav'].join('')
+    console.log('Writing new recording file at:', fileName);
 
-// Create write stream.
-const fileStream = fs.createWriteStream(fileName, { encoding: 'binary' });
+    // Create write stream.
+    const fileStream = fs.createWriteStream(fileName, { encoding: 'binary' });
 
-// Start and write to the file.
-audioRecorder.start().stream().pipe(fileStream);
+    // Start and write to the file.
+    audioRecorder.start().stream().pipe(fileStream);
 
-// Keep process alive.
-process.stdin.resume();
-console.warn('Press ctrl+c to exit.');
+    // Keep process alive.
+    process.stdin.resume();
+    console.warn('Press ctrl+c to exit.');
 
-// Keep alive for 3 hours
-setTimeout(() => {
-  process.exit()
-}, 1000 * 60 * 60 * 3)
+    // Keep alive for 3 hours
+    setTimeout(() => {
+      process.exit()
+    }, 1000 * 60 * 60 * 3)
+
+	},
+	null,
+	true, // if false, have to use job.run()
+);
