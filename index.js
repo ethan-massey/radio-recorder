f32fbab54985be1a5038c1f6a873e00bd328da57
Ethan Massey <ethanmassey@Ethans-MacBook-Pro-2.local>
2024-01-11 00:11:19 -0500

    just upload file to s3

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ const { createRecording } = require('./recordService')
 const { uploadFileToS3 } = require('./s3Service')
 require('log-timestamp')
 
-createRecording().then((fileName) => {
-    uploadFileToS3(fileName)
-})
+uploadFileToS3('output.aac')
 
