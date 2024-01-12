431cb3a0c5a0b66515b5be7146471dbc169aedf3
Ethan Massey <ethanmassey@Ethans-MacBook-Pro-2.local>
2024-01-12 12:00:28 -0500

    add log

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ try {
     var files = fs.readdirSync('/home/pi/Desktop/radio-recorder/recordings/');
     // upload to s3
     files.forEach(file => {
+        console.log(`Found file ${file} in recordings directory. Attempting upload.`)
         uploadFileToS3(file)
     })
 } catch (err) {
