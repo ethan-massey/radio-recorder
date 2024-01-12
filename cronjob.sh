8c5357b0685e203268ecc1f2fc8d33037eb65201
Ethan Massey <ethanmassey@Ethans-MacBook-Pro-2.local>
2024-01-12 16:41:47 -0500

    update scripts

--- cronjob.sh
+++ cronjob.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /home/ethan/Desktop/radio-recorder
+ffmpeg -i http://stream.revma.ihrhls.com/zc2525 -t 00:02:00 /home/ethan/Desktop/radio-recorder/recordings/$(date +%Y-%m-%dT%H-%M-%S).mp3
 npm run start-cron >> log
