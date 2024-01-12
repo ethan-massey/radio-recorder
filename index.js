7a6087e26186f937ebcac0f8da272528de20ee99
Ethan Massey <ethanmassey@Ethans-MacBook-Pro-2.local>
2024-01-12 11:59:00 -0500

    refactor index

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const { uploadFileToS3 } = require('./s3Service')
 require('log-timestamp')
+const fs = require('fs');
 
-uploadFileToS3('output.aac')
+try {
+    // get recording file names
+    var files = fs.readdirSync('/home/pi/Desktop/radio-recorder/recordings/');
+    // upload to s3
+    files.forEach(file => {
+        uploadFileToS3(file)
+    })
+} catch (err) {
+    console.error(err)
+}
