f16dd816508d62a40b42c240b2ff96a24bbab5b6
Ethan Massey <ethanmassey@Ethans-MacBook-Pro-2.local>
2024-01-18 13:10:55 -0500

    add log for case of no files found to upload

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const { uploadFileToS3 } = require('./s3Service')
-require('log-timestamp')
-const fs = require('fs');
+const { uploadFileToS3 } = require("./s3Service");
+require("log-timestamp");
+const fs = require("fs");
 
 try {
   // get recording file names
-  var files = fs.readdirSync('recordings');
-  // upload to s3
-  files.forEach(file => {
-    console.log(`Found file ${file} in recordings directory. Attempting upload.`)
-    uploadFileToS3(file)
-  })
+  var files = fs.readdirSync("recordings");
+
+  if (files.length === 0) {
+    console.log("No files found in recordings directory. Exiting...");
+  } else {
+    // upload to s3
+    files.forEach((file) => {
+      console.log(
+        `Found file ${file} in recordings directory. Attempting upload.`
+      );
+      uploadFileToS3(file);
+    });
+  }
 } catch (err) {
-  console.log(err)
+  console.log(err);
 }
